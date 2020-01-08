42d5789778278c2ae9e49af8b3cfb6a44965e4ec
Dmitriy65 <mr.dima19980@gmail.com>
2020-01-08 18:49:18 +0300

    Updates

---
+++ precache-manifest.45c38e9ed09b8cd180810ac5554eed3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2ea6f611096c61eb629f98f177b34f",
+    "url": "/Canvas-Paint-App/index.html"
+  },
+  {
+    "revision": "9d9bf451bffcbd00d4da",
+    "url": "/Canvas-Paint-App/static/css/main.01205144.chunk.css"
+  },
+  {
+    "revision": "567cdd83cb30818c94fe",
+    "url": "/Canvas-Paint-App/static/js/2.c7ae556f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Canvas-Paint-App/static/js/2.c7ae556f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d9bf451bffcbd00d4da",
+    "url": "/Canvas-Paint-App/static/js/main.74d94403.chunk.js"
+  },
+  {
+    "revision": "499f2852afcbabc04e43",
+    "url": "/Canvas-Paint-App/static/js/runtime-main.ef4db5c9.js"
+  }
+]);
